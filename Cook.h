4e3a6110e56b7fb92f2fe2f204019b8d094a5d58
shroukAli01 <Shroukali2006@gmail.com>
2025-11-22 13:19:58 +0200

    Update Cook.h

--- Cook.h
+++ Cook.h
@@ -1,20 +1,66 @@
 #pragma once
-
 #include "..\Defs.h"
 
-#pragma once
+
+class Order;
+
 class Cook
 {
-	int ID;
-	ORD_TYPE type;	//for each order type there is a corresponding type (VIP, Normal, Vegan)
-	int speed;		//dishes it can prepare in one clock tick (in one timestep)
+private:
+    int ID;
+    ORD_TYPE type;      // (V/N/V)
+    int speed;        
+
+   
+    int BO;                 // Break Order count
+    int BreakDuration;     
+    int BreakRemaining;     
+    int OrdersSinceBreak;
+
+    bool Available;
+
+    Order* CurrentOrder;   
+    int RemainingTime;      
 
 public:
-	Cook();
-	virtual ~Cook();
-	int GetID() const;
-	ORD_TYPE GetType() const;
-	void setID(int);
-	void setType(ORD_TYPE) ;
 
+    // ---------- Constructor ----------
+    Cook(int id = 0, ORD_TYPE t = TYPE_NRM, int speed = 0,
+         int bo = 0, int breakDur = 0);
+
+    //  Destructor 
+    ~Cook();
+
+    // Setters 
+    void setID(int id);
+    void setType(ORD_TYPE t);
+    void SetSpeed(int s);
+    void SetBO(int bo);
+    void SetBreakDuration(int bd);
+
+    // Getters 
+    int GetID() const;
+    ORD_TYPE GetType() const;
+    int GetSpeed() const;
+    int GetBO() const;
+    int GetBreakDuration() const;
+    int GetBreakRemaining() const;
+
+    bool IsAvailable() const;
+    void SetAvailable(bool a);
+
+    //  Break Handling 
+    void StartBreak();
+    void UpdateBreak();
+    bool InBreak() const;
+    void ResetBreak();
+
+    //  Order Handling 
+    void AssignOrder(Order* o, int prepTime);
+    void FreeOrder();
+    Order* GetCurrentOrder() const;
+    bool IsBusy() const;
+
+    void OrderAssigned();        // for BO counter
+    void UpdateCooking();        // decrease remaining time
 };
